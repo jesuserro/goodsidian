8644d0cbb14b8c6e1a8aeaa72cdf7dca167868d2
olagato <olagato@gmail.com>
2022-05-06 13:12:47 +0200

    feat(Book data): added editorial

--- get-books.sh
+++ get-books.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# see: https://unix.stackexchange.com/questions/277861/parse-xml-returned-from-curl-within-a-bash-script
+
 . ./goodreads.cfg
 
 url="$urlbase/review/list_rss/$user?key=$key&shelf=$shelf"
@@ -18,6 +20,7 @@ if (( "$bookamount" == 0 )); then
 fi
 
 xpathBook="GoodreadsResponse/book"
+xpathAuthor="GoodreadsResponse/book/authors/author[1]"
 
 # Start the loop for each book
 for (( i = 0 ; i < ${bookamount} ; i++ ))
@@ -34,9 +37,27 @@ do
 
   xmlBook=$(curl -s $urlBook)
 
+  # LIBRO
   title=$(echo $xmlBook | xmllint --xpath "//$xpathBook/title[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
-  publication_year=$(echo $xmlBook | xmllint --xpath "//$xpathBook/publication_year[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  image_url=$(echo $xmlBook | xmllint --xpath "//$xpathBook/image_url[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  description=$(echo $xmlBook | xmllint --xpath "//$xpathBook/description[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  publisher=$(echo $xmlBook | xmllint --xpath "//$xpathBook/publisher[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  # isbn=$(echo $xmlBook | xmllint --xpath "//$xpathBook/isbn[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  # isbn13=$(echo $xmlBook | xmllint --xpath "//$xpathBook/isbn13[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  # kindle_asin=$(echo $xmlBook | xmllint --xpath "//$xpathBook/kindle_asin[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  # publication_year=$(echo $xmlBook | xmllint --xpath "//$xpathBook/publication_year[1]/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
 
-  echo "$bookid -> $title -> $publication_year"
+  # AUTOR
+  # authorId=$(echo $xmlBook | xmllint --xpath "//$xpathAuthor/id/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  # urlAuthor="$urlbase/author/show.xml?key=$apikey&id=$authorId"
+
+  authorName=$(echo $xmlBook | xmllint --xpath "//$xpathAuthor/name/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  authorImage=$(echo $xmlBook | xmllint --xpath "//$xpathAuthor/image_url/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+  authorLink=$(echo $xmlBook | xmllint --xpath "//$xpathAuthor/link/text()" - | sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' )
+
+
+  # echo "$bookid -> $title -> $kindle_asin -> $isbn -> $isbn13 -> $publication_year"
+  # echo "$bookid -> $title -> $authorName -> $authorLink"
+  echo "$bookid -> $publisher"
 
 done
