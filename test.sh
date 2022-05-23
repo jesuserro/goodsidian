6716279bdf2a4567ab488496255efaf38174fd39
olagato <olagato@gmail.com>
2022-05-23 08:41:37 +0200

    test(test file): updated converting html link to obsidian link

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-link='<a href="www.link.com">Link</a>'
+# see: https://stackoverflow.com/questions/40273926/convert-html-links-to-markdown-using-command-sed
+
+# link='<a href="www.link.com">Link</a>'
 link='<a href="https://www.goodreads.com/author/show/4443885._the_Catholic_Church" title=" the Catholic Church" rel="nofollow noopener"> the Catholic Church</a>'
 
-echo "${link}" | sed 's/<a href="\(.*\)">\(.*\)<\/a>/[\2](\1)/'
+# echo "${link}" | sed -r 's/.*href="([^"]+).*/[Link](\1)/g' # Pilla el href
+# echo "${link}" | sed 's|<a href="\([^\"]+\)".*>\(.*\)<\/a>|[\2](\1)|g'
+# echo "${link}" | sed 's|<a href="\(.*\)">\(.*\)<\/a>|[\2](\1)|g'
+# echo "${link}" | sed -r 's/<a href="(.*)">(.*)<.*/[\2](\1)/g' # Pilla texto link
+echo "${link}" | sed -r 's/<a href="([^"]+).*>(.*)<.*/[\2](\1)/g' # OK
